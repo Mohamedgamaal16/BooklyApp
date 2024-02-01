0ae9cb21458507270dc81685af89b204ff665264
mohamedgamaal16 <Mahooomar@yahoo.com>
2024-02-01 02:13:35 +0200

    mvvm design pattern and make splash feature

---
+++ lib/features/splash/presention/views/splash_view.dart
@@ -0,0 +1,11 @@
+import 'package:bookly_gemy/features/splash/presention/views/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplashViewBody();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_gemy/constans.dart';
+import 'package:bookly_gemy/features/splash/presention/views/splash_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      home: const SplashView(),
+    );
+  }
+}
