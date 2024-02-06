483bf9ab8540c9767c7adfa88af958fe20ac8b23
mohamedgamaal16 <Mahooomar@yahoo.com>
2024-02-06 23:44:43 +0200

    create search view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,11 +1,13 @@
 import 'package:bookly_gemy/features/home_page/presention/views/book_detailes_view.dart';
 import 'package:bookly_gemy/features/home_page/presention/views/home_view.dart';
+import 'package:bookly_gemy/features/search/presention/views/search_view.dart';
 import 'package:bookly_gemy/features/splash/presention/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter{
   static const kHomeView = '/homeView';
     static const kBookDetailesView = '/bookDetailesView';
+    static const kSearchView = '/searchView';
 
  static final GoRouter router = GoRouter(routes: [
   GoRoute(
@@ -20,6 +22,9 @@ abstract class AppRouter{
     GoRoute(
       path: kBookDetailesView,
       builder: (context, state) => const BookDetailesView(),
+    ),GoRoute(
+      path: kSearchView,
+      builder: (context, state) => const SearchView(),
     ),
 ]);
 }
---
+++ lib/features/search/presention/views/search_view.dart
@@ -0,0 +1,11 @@
+import 'package:bookly_gemy/features/search/presention/views/widgets/search_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: SafeArea(child: SearchViewBody()),);
+  }
+}
