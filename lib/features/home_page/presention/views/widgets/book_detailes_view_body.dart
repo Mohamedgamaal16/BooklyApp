6d6d05b6755aed153131959c49e154665639f501
mohamedgamaal16 <Mahooomar@yahoo.com>
2024-02-06 17:48:25 +0200

    add custom scroll view

--- lib/features/home_page/presention/views/widgets/book_detailes_view_body.dart
+++ lib/features/home_page/presention/views/widgets/book_detailes_view_body.dart
@@ -12,57 +12,68 @@ class BookDetailesViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Column(
-        children: [
-          const CustomBookDetailesAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .25),
-            child: const CustomBookImage(),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: Column(
+              children: [
+                const CustomBookDetailesAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * .25),
+                  child: const CustomBookImage(),
+                ),
+                const SizedBox(
+                  height: 43,
+                ),
+                const Text(
+                  'The Jungle Book',
+                  style: Styles.textStyle30,
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Opacity(
+                    opacity: .7,
+                    child: Text(
+                      'Rubyard kipling',
+                      style: Styles.textStyle20.copyWith(
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w500),
+                    )),
+                const SizedBox(
+                  height: 14,
+                ),
+                const BookRatingItem(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const BooksActionsButton(),
+                const Expanded(
+                  child: SizedBox(
+                    height: 30,
+                  ),
+                ),
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'you may also like',
+                      style: Styles.textStyle14
+                          .copyWith(fontWeight: FontWeight.w700),
+                    )),
+                const SizedBox(
+                  height: 8,
+                ),
+                const BooksDetailesListView(),
+              ],
+            ),
           ),
-          const SizedBox(
-            height: 43,
-          ),
-          const Text(
-            'The Jungle Book',
-            style: Styles.textStyle30,
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          Opacity(
-              opacity: .7,
-              child: Text(
-                'Rubyard kipling',
-                style: Styles.textStyle20.copyWith(
-                    fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
-              )),
-          const SizedBox(
-            height: 14,
-          ),
-          const BookRatingItem(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const BooksActionsButton(),
-          const SizedBox(
-            height: 20,
-          ),
-          Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'you may also like',
-                style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w700),
-              )),
-       const SizedBox(height: 8,), const BooksDetailesListView(),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
-
-
--- lib/features/home_page/presention/views/widgets/books_detailes_list_view.dart
+++ lib/features/home_page/presention/views/widgets/books_detailes_list_view.dart
@@ -7,7 +7,7 @@ class BooksDetailesListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * .14,
+      height: MediaQuery.of(context).size.height * .17,
       child: ListView.builder(
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) {
